2250ab9ae071c8ca3f6a5ff1177467ad567e5d46
Mohamed mkhy <this-mkhy>
2022-03-16 15:37:31 +0200

    update readme

--- src/projects/1-counter/components/Counter/Counter.js
+++ src/projects/1-counter/components/Counter/Counter.js
@@ -1,32 +1,78 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+// import Code from '../Code/Code';
+// import Desc from '../Desc/Desc';
+// import './counter.css'
+// import { Link } from "react-router-dom";
+
+// function Counter () {
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => setCount(count + 1);
+//   const decrement = () => setCount(count - 1);
+ 
+//   return (
+//     <div className="container">
+//       <div className="counter">
+//         <h1>Counter</h1>
+//         <button onClick={increment}>Increase + </button>
+//         <h2>{count}</h2>
+//         <button onClick={decrement}>Decrease - </button>
+//       </div>
+//       <div className="desc">
+//         <Desc />
+//         <Link to="/">Home</Link>
+//       </div>
+//       <div className="code">
+//         <Code />
+//       </div> 
+//     </div>
+//   )
+// }
+
+// export default Counter
+
+import React, { Component, useState } from 'react'
 import Code from '../Code/Code';
 import Desc from '../Desc/Desc';
 import './counter.css'
 import { Link } from "react-router-dom";
 
-function Counter () {
-  const [count, setCount] = useState(0);
+class Counter extends Component{
+  state = {
+    count : 0
+  }
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
- 
-  return (
-    <div className="container">
-      <div className="counter">
-        <h1>Counter</h1>
-        <button onClick={increment}>Increase + </button>
-        <h2>{count}</h2>
-        <button onClick={decrement}>Decrease - </button>
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  
+  render(){
+    return (
+      <div className="container">
+        <div className="counter">
+          <h1>Counter</h1>
+          <button onClick={this.increment}>Increase + </button>
+          <h2>{this.state.count}</h2>
+          <button onClick={this.decrement}>Decrease - </button>
+        </div>
+        <div className="desc">
+          <Desc />
+          <Link to="/">Home</Link>
+        </div>
+        <div className="code">
+          <Code />
+        </div> 
       </div>
-      <div className="desc">
-        <Desc />
-        <Link to="/">Home</Link>
-      </div>
-      <div className="code">
-        <Code />
-      </div> 
-    </div>
-  )
+    )
+  }
 }
 
 export default Counter
