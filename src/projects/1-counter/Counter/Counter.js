736881e0a165118e37c619257463f6d02cf8be6a
Andersen <rickansenn@gmail.com>
2022-10-27 22:39:56 +0800

    Issue 21 Style: Implemented CSS Modules to Counter App

--- src/projects/1-counter/Counter/Counter.js
+++ src/projects/1-counter/Counter/Counter.js
@@ -38,9 +38,18 @@ import Code from '../Code/Code';
 import Desc from '../Desc/Desc';
 
 import './../../projects.css' 
-import './counter.css'
+import styles from './counter.module.css'
 
+  const {
+    container, 
+    projectName, 
+    counterBtn, 
+    counterNumber, 
+    description, 
+    homeLink, 
+    codeRight } = styles
 class Counter extends Component{
+
   state = {
     count : 0
   }
@@ -59,19 +68,19 @@ class Counter extends Component{
   
   render(){
     return (
-      <div className="container">
-        <div className="projectName">
+      <div className={container}>
+        <div className={projectName}>
           <h2 style={{fontSize: '30px', color: '#3498db', marginTop: '10px'}}>Counter</h2>
-          <button className="counterBtn" onClick={this.increment}>Increase + </button>
-          <h2 className="counterNumber">{this.state.count}</h2>
-          <button className="counterBtn" onClick={this.decrement}>Decrease - </button>
+          <button className={counterBtn} onClick={this.increment}>Increase + </button>
+          <h2 className={counterNumber}>{this.state.count}</h2>
+          <button className={counterBtn} onClick={this.decrement}>Decrease - </button>
         </div>
-        <div className="description">
+        <div className={description}>
           <Desc />
-          <Link className='homeLink' to="/">Home</Link>
-          <Link className='homeLink' to="/colors" title='Display Colors'>Next</Link>
+          <Link className={homeLink} to="/">Home</Link>
+          <Link className={homeLink} to="/colors" title='Display Colors'>Next</Link>
         </div>
-        <div className="codeRight">
+        <div className={codeRight}>
           <Code />
         </div> 
       </div>
