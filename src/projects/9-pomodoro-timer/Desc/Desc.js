388b4587fccce1b1ad92f0b6bfe79c2124640c15
thismkhy <mkhy88519@gmail.com>
2023-10-06 07:35:09 +0300

    fix : switch to next and previous pages

--- src/projects/9-pomodoro-timer/Desc/Desc.js
+++ src/projects/9-pomodoro-timer/Desc/Desc.js
@@ -6,8 +6,10 @@ class Desc extends Component {
         return (
             <div className="desc">
                 <h1 style={{ color: '#3498db' }}>How to code the Pomodoro Timer ?</h1>
-                <mark>Posted on Mar 16, 2022</mark>
+                <mark>Posted on Oct 06, 2023</mark>
                 <h3>Remember this concept, <br />Start s-m-a-l-l to create something BIG</h3>
+                <p>Coming Soon</p>
+
             </div>
         )
     }
